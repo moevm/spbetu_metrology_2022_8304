e09a5e204ed98e6058e6624e30baf64f6cc50a22
AlexRyzhickov <makseljoinb@gmail.com>
2022-03-23 16:59:29 +0300

    sampler screen without `printf`

--- lab3/main.c
+++ lab3/main.c
@@ -8,18 +8,19 @@
 
 void linfit2(float *x, float *y, float  *y_calc) {
     float sum_x = 0, sum_y = 0, sum_xy = 0, sum_x2 = 0, sum_y2 = 0;
-    float sxy, sxx, syy;
+    float xi, yi, sxy, sxx, syy;
     SAMPLE;
     for (int i = 0; i < n; ++i) {
-        
-        
-        sum_x += x[i];
-        sum_y += y[i];
-        sum_xy += x[i] * y[i];
-        sum_x2 += x[i] * x[i];
-        sum_y2 += y[i] * y[i];
+        xi = x[i];
+        yi = y[i];
+        sum_x += xi;
+        sum_y += yi;
+        sum_xy += xi * yi;
+        sum_x2 += xi * xi;
+        sum_y2 += yi * yi;
+        SAMPLE;
     }
-    SAMPLE;
+    
     sxx = sum_x2 - sum_x * sum_x / n;
     sxy = sum_xy - sum_x * sum_y / n;
     syy = sum_y2 - sum_y * sum_y / n;
@@ -27,7 +28,8 @@ void linfit2(float *x, float *y, float  *y_calc) {
     float a = ((sum_x2 * sum_y - sum_x * sum_xy) / n) / sxx;
     SAMPLE;
     for (int i = 0; i < n; ++i) {
-        *(y_calc + i) = a + b * x[i];
+    	y_calc[i] = a + b * x[i];
+        SAMPLE;
     }
 }
 
@@ -41,24 +43,28 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; i++) {
         x[i] = (float) (rand() % 100) / 100;
         y[i] = (float) (rand() % 100) / 100;
+        SAMPLE;
     }
     SAMPLE;
     linfit2(x, y, y_calc);
-    SAMPLE;
-    printf("x: ");
-    for (int i = 0; i < n; ++i) {
-        printf("%f ;", x[i]);
-    }
-    SAMPLE;
-    printf("\ny: ");
-    for (int i = 0; i < n; ++i) {
-        printf("%f ;", y[i]);
-    }
-    SAMPLE;
-    printf("\ny_calc: ");
-    for (int i = 0; i < n; ++i) {
-        printf("%f ;", y_calc[i]);
-    }
+    //SAMPLE;
+    //printf("x: ");
+    //for (int i = 0; i < n; ++i) {
+    //    printf("%f ;", x[i]);
+    //    SAMPLE;
+    //}
+    //SAMPLE;
+    //printf("\ny: ");
+    //for (int i = 0; i < n; ++i) {
+    //    printf("%f ;", y[i]);
+    //    SAMPLE;
+    //}
+    //SAMPLE;
+    //printf("\ny_calc: ");
+    //for (int i = 0; i < n; ++i) {
+    //    printf("%f ;", y_calc[i]);
+    //    SAMPLE;
+    //}
     SAMPLE;
     free(x);
     free(y);
