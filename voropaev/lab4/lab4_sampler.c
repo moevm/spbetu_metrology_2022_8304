f5df533ec9883915c943fa00d8c10a20b2d45d40
AntonV <42892405+StiXs221@users.noreply.github.com>
2022-03-26 17:21:44 +0300

    lab4 ready

---
+++ voropaev/lab4/lab4_sampler.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+#include "sampler.h"
+
+
+
+float fx(float x){
+    return 1/sqrtf(x);
+}
+
+void trapez(float lower, float upper, double tol, float *sum){
+    float x = 0, end_sum = 0, mid_sum = 0, sum1 = 0;
+    float delta_x = 0;
+    int i = 0, pieces = 1;
+    delta_x = (float)(upper-lower)/pieces;
+    end_sum = fx(lower) + fx(upper);
+    *sum = end_sum*delta_x/2;
+    //printf("1 %lf\n", *sum);
+    mid_sum = 0;
+    SAMPLE;
+    do{
+		SAMPLE;
+        pieces = pieces * 2;
+        sum1 = *sum;
+		
+        delta_x = (float)(upper - lower) / pieces;
+		SAMPLE;
+        for (i = 1; SAMPLE, i <= pieces / 2; i++) {
+			SAMPLE;
+			x = lower+delta_x*(float)(2*i - 1);
+            mid_sum = mid_sum + fx(x);
+			SAMPLE;
+        }
+		SAMPLE;
+        *sum = (end_sum + 2*mid_sum) * delta_x * 0.5;
+        //printf("%d %e\n", pieces, *sum);
+		SAMPLE;
+    }while(fabsf(*sum - sum1) > fabsf(tol*(*sum)));
+    SAMPLE;
+}
+
+int main(int pargc, char **argv) {
+	sampler_init(&pargc, argv);
+	SAMPLE;
+    float lower = 1;
+    float upper = 9;
+    //printf("\n");
+    const double tol = 1.0e-6;
+    float sum = 0;
+    
+    trapez(lower, upper, tol, &sum);
+    //printf("\nsum = %lf", sum);
+    SAMPLE;
+	return 0;
+}
