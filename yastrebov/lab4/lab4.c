be62ca0bca8975b80bdb0d83a443225c41e34f4d
Ivan Yastrebov <vanka998@mail.ru>
2022-04-16 23:37:48 +0300

    lab 4 done

---
+++ yastrebov/lab4/lab4.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "sampler.h"
+const int max = 10;
+
+int done;
+
+void swap(int* p, int* q)
+{
+    float hold;
+    hold = *p;
+    *p = *q;
+    *q = hold;
+}
+
+void sort(int a[max], int n){
+  int jump = n;
+  int i;
+  SAMPLE;
+  while (jump > 1) 
+  { SAMPLE;
+    jump = jump / 2;
+    do
+    {
+        SAMPLE;
+	    int done = 1;
+        SAMPLE;
+	    for(int j = 0;SAMPLE, j < n; ++j)
+	    {
+            SAMPLE;
+	        i = j + jump;
+            SAMPLE;
+	        if (a[j] > a[i])
+	        {
+                SAMPLE;
+		        swap(&(a[j]),&(a[i]));
+		        done = 0;
+                SAMPLE;
+	        }
+            SAMPLE;
+	    } SAMPLE;
+	} while (done); SAMPLE;
+  }
+  SAMPLE;
+  return;
+}		
+
+void write_arr(int a[max])
+{
+    SAMPLE;
+    for (int i = 0;SAMPLE, i < max; ++i)
+    {
+        SAMPLE;
+        //printf("%d\n", a[i]);
+        SAMPLE;
+    } SAMPLE;
+}
+
+
+int main(int pargc, char **argv) {
+ sampler_init(&pargc, argv);
+ SAMPLE;
+ int n = max;
+ srand(time(NULL));
+ int x[max];
+ SAMPLE;
+ for(int i = 0;SAMPLE, (i < n); ++i)
+ {SAMPLE;
+     x[i] = rand() % 100;
+
+ SAMPLE;}
+ SAMPLE;
+ sort( x,n );
+ write_arr(x);
+ SAMPLE;
+}
