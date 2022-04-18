36a3516830dd0c4aec6cbc1cb075665858efb89e
NamYoSeb <42902568+NamYoSeb@users.noreply.github.com>
2022-04-18 03:20:25 +0300

    Nam lab4

--- nam/lab4/lab1.c
+++ nam/lab4/lab1.c
@@ -1,95 +1,77 @@
-// Online C compiler to run C program online
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-int max = 80;
+#include <time.h>
 
-void swap(int *p, int *q){
-    int hold;
-    hold = *p; 
-    *p = *q; 
+#define max 10
+
+void swap(double* p, double* q)
+{
+    double hold = *p;
+    *p = *q;
     *q = hold;
 }
 
-void sort(int x[max], int n){
-    int left[20], right[20], i, j, sp, mid;
-    float pivot;
-    left[0] = 0; 
-    right[0] = n - 1; 
-    sp = 0; 
-
-    while (sp > -1){ 
-        if (left[sp] >= right[sp]){
-            sp--;
-        }  
-        else {
-            i = left[sp]; 
-            j = right[sp]; 
-            pivot = x[j]; 
-            mid = (i + j) / 2; 
-            if ((j - i) > 5){
-                if (((x[mid] < pivot) && (x[mid] > x[i])) || ((x[mid] > pivot) && (x[mid] < x[i]))){ 
-                    swap(&(x[mid]), &(x[j]));
-                }
-                else if(((x[i] < x[mid]) && (x[i] > pivot)) || ((x[i] > x[mid]) && (x[i] < pivot))){
-                    swap(&(x[i]), &(x[j])); 
-                }
-            } 
-         
-            pivot = x[j]; 
+void sort(double* x, int n)
+{
+    int left[20];
+    int right[20];
+    int i, j, sp, mid;
+    double pivot;
+	
+    left[0] = 0;
+    right[0] = n-1;
+    sp = 0;
     
-        while (i < j){ 
-            while (x[i] < pivot){
-                i++; 
+    while (sp > -1){
+        if (left[sp] >= right[sp]) {
+            sp--;
+        } else {
+	        i = left[sp];
+	        j = right[sp];
+	        pivot = x[j];
+	        mid = (i + j) / 2; //div
+	        
+	        if ((j - i) > 5){
+	            if (((x[mid] < pivot) && (x[mid] > x[i])) || ((x[mid] > pivot) && (x[mid] < x[i]))){
+		            swap(x + mid, x + j);
+	            } else {
+	                if (((x[i] < x[mid]) && (x[i] > pivot)) || ((x[i] > x[mid]) && (x[i] < pivot)))
+		                swap(x + i, x + j);
+	            }
+	        }
+	        
+            pivot = x[j];
+            while (i < j) {
+	            while (x[i] < pivot)
+	                i++;
+	            j = j - 1;
+	            while ((i < j) && (pivot < x[j]))
+	                j--;
+	            if (i < j) 
+	                swap(x + i, x + j);
             }
             
-            j--;
+            j = right[sp];
+            swap(x + i, x + j);
             
-            while ((i < j) && (pivot < x[j])){ 
-                j--; 
+            if (i - left[sp] >= right[sp] - i){
+	            left[sp + 1] = left[sp];
+	            right[sp + 1] = i - 1;
+	            left[sp] = i + 1;
+            } else {
+	            left[sp + 1] = i + 1;
+	            right[sp + 1] = right[sp];
+	            right[sp] = i - 1;
             }
-            
-            if (i < j){
-                    swap(&(x[i]), &(x[j]));
-                } 
-        } 
-        
-        j = right[sp];
-        
-        swap(&(x[i]), &(x[j])); 
-        
-        if (i - left[sp] >= right[sp] - i){  
-            left[sp + 1] = left[sp]; 
-            right[sp + 1] = i - 1; 
-            left[sp] = i + 1;
-        }
-        else{
-        left[sp + 1] = i + 1; 
-        right[sp + 1] = right[sp]; 
-        right[sp] = i - 1 ;
-        }
-        
-        sp++;
+            sp++;
         }
     }
 }
 
-
-int main() {
+int main()
+{
     int n = max;
-    int x[n];
-    
-    srand(time(NULL));
-    
-    for (int i = 0; i < n; i++){
-    x[i] = rand()%100;
-    }
-    
+    double x[max] = {2.1, 3.0, 0.3, 5.6, 2.7, 1.0, 45.9, 10.0, 94.2, 0.01};
     sort(x, n);
-    
-    for(int i = 0; i < n; i++){
-        printf("%d ", x[i]);
-    }
-    
     return 0;
 }
--- nam/lab4/lab1_2.c
+++ nam/lab4/lab1_2.c
@@ -1,114 +1,104 @@
-// Online C compiler to run C program online
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 #include "sampler.h"
 
-int max = 80;
+#define max 10
 
-void swap(int *p, int *q){
-    int hold;
-    hold = *p; 
-    *p = *q; 
+void swap(double* p, double* q)
+{
+    double hold = *p;
+    *p = *q;
     *q = hold;
 }
 
-void sort(int x[max], int n){
-    int left[20], right[20], i, j, sp, mid;
-    float pivot;
-    left[0] = 0; 
-    right[0] = n - 1; 
-    sp = 0; 
-	SAMPLE;
-    while (sp > -1){ 
-		SAMPLE;
-        if (left[sp] >= right[sp]){
-			SAMPLE;
+void sort(double* x, int n)
+{
+    int left[20];
+    int right[20];
+    int i, j, sp, mid;
+    double pivot;
+    SAMPLE;	
+    left[0] = 0;
+    right[0] = n-1;
+    sp = 0;
+    
+    SAMPLE;
+    while (sp > -1){
+    	SAMPLE;
+        if (left[sp] >= right[sp]) {
             sp--;
-			SAMPLE;
-        }  
-        else {
-		SAMPLE;
-            i = left[sp]; 
-            j = right[sp]; 
-            pivot = x[j]; 
-            mid = (i + j) / 2; 
             SAMPLE;
-			if ((j - i) > 5){
-				SAMPLE;
-                if (((x[mid] < pivot) && (x[mid] > x[i])) || ((x[mid] > pivot) && (x[mid] < x[i]))){ 
-                    swap(&(x[mid]), &(x[j]));
-                }
-                else if(((x[i] < x[mid]) && (x[i] > pivot)) || ((x[i] > x[mid]) && (x[i] < pivot))){
-                    swap(&(x[i]), &(x[j])); 
-                
-				}
-				SAMPLE;
-            } 
-         SAMPLE;
-            pivot = x[j]; 
+        } else {
 		SAMPLE;
-        while (i < j){ 
-			SAMPLE;
-            while (x[i] < pivot){
-                i++; 
+	        i = left[sp];
+	        j = right[sp];
+	        pivot = x[j];
+	        mid = (i + j) / 2; //div
+	        SAMPLE;
+	        if ((j - i) > 5){
+	            SAMPLE;
+	            if (((x[mid] < pivot) && (x[mid] > x[i])) || ((x[mid] > pivot) && (x[mid] < x[i]))){
+		            swap(x + mid, x + j);
+		            SAMPLE;
+	            } else {
+	                SAMPLE;
+	                if (((x[i] < x[mid]) && (x[i] > pivot)) || ((x[i] > x[mid]) && (x[i] < pivot))){
+		                swap(x + i, x + j);
+		                SAMPLE;
+					}
+	            }
+	        }
+	        
+            pivot = x[j];
+            SAMPLE;
+            while (i < j){
+            	SAMPLE;
+	            while (x[i] < pivot){
+	                i++;
+	                SAMPLE;
+	            }
+	            j = j - 1;
+	            SAMPLE;
+	            while ((i < j) && (pivot < x[j])){
+	                j--;
+	                SAMPLE;
+	            }
+	            SAMPLE;
+	            if (i < j){ 
+	                swap(x + i, x + j);
+	                SAMPLE;
+	            }
             }
-            
-            j--;
-            
-            while ((i < j) && (pivot < x[j])){ 
-                j--; 
+            SAMPLE;
+            j = right[sp];
+            SAMPLE;
+            swap(x + i, x + j);
+            SAMPLE;
+            if (i - left[sp] >= right[sp] - i){
+	            left[sp + 1] = left[sp];
+	            right[sp + 1] = i - 1;
+	            left[sp] = i + 1;
+	            SAMPLE;
+            } else {
+	            left[sp + 1] = i + 1;
+	            right[sp + 1] = right[sp];
+	            right[sp] = i - 1;
+	            SAMPLE;
             }
-            
-            if (i < j){
-                    swap(&(x[i]), &(x[j]));
-                } 
-			SAMPLE;
-        } 
-        SAMPLE;
-        j = right[sp];
-        
-        swap(&(x[i]), &(x[j])); 
-        SAMPLE;
-        if (i - left[sp] >= right[sp] - i){  
-            left[sp + 1] = left[sp]; 
-            right[sp + 1] = i - 1; 
-            left[sp] = i + 1;
-        SAMPLE;
-		}
-        else{
-			SAMPLE;
-        left[sp + 1] = i + 1; 
-        right[sp + 1] = right[sp]; 
-        right[sp] = i - 1 ;
-        SAMPLE;
-		}
-        SAMPLE;
-        sp++;
+            sp++;
+            SAMPLE;
         }
-		SAMPLE;
     }
-	SAMPLE;
+    SAMPLE;
 }
 
-
-int main(int pargc, char **argv) {
-	sampler_init(&pargc, argv);
-	
+int main(int argc, char **argv)
+{
+    sampler_init(&argc, argv);
+    
     int n = max;
-    int x[n];
-    
-    srand(time(NULL));
-    
-    for (int i = 0; i < n; i++){
-    x[i] = rand()%100;
-    }
-    
+    double x[max] = {2.1, 3.0, 0.3, 5.6, 2.7, 1.0, 45.9, 10.0, 94.2, 0.01};
     sort(x, n);
-    
-    for(int i = 0; i < n; i++){
-        printf("%d ", x[i]);
-    }
-    
     return 0;
 }
